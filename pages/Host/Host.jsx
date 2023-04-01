b4d08b3b511c08234b08ce9c1003b366c68e9190
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2023-04-01 11:01:48 -0700

    initial commits

---
+++ pages/Host/Host.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Host() {
+    return(
+        <>
+            <div className="header"> 
+                
+            </div>
+        </>
+    )
+}
