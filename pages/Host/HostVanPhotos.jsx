df34f5ce406d29368dfceb9624c83408b7b540cc
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-07-16 17:12:07 -0700

    deleted

--- pages/Host/HostVanPhotos.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useOutletContext } from "react-router-dom";
-
-export default function HostVanPhotos() {
-  const { imageUrl } = useOutletContext();
-  return <img src={imageUrl} alt="van" className="host-van-detail-image" />;
-}
