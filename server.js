9a9b56cc062f6842e3a539dcd9d1b2486d9056b3
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-07-17 23:16:51 -0700

    crud code for employee

--- server.js
+++ server.js
@@ -1,79 +1,42 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
-// works as waterfall so this will run first then app.get()
+// custom middleware logger
+app.use(logger);
 
-// custom middleware
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path} custom`);
-  next();
-});
-
-// built-in middleware for handling form data
+// built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
-// handling json data
+// built-in middleware for json
 app.use(express.json());
 
 //serve static files
-app.use(express.static(path.join(__dirname, "./public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
-// added router for this route
-app.use("/subdir", require("./routes/subdir"));
+// routes
+app.use("/", require("./routes/root"));
 app.use("/employees", require("./routes/api/employees"));
 
-// app.get("^/$|/index(.html)?", (req, res) => {
-//   //   Current Directory: Suppose your script is located in /home/user/project/server.js.
-//   // __dirname: The value of __dirname would be /home/user/project.
-//   //  Express resolves the relative path ./views/index.html from the root directory /home/user/project, resulting in the absolute path /home/user/project/views/index.html.
-//   //res.sendFile("./views/index.html", { root: __dirname });
-//   //res.sendFile('./views/index.html', { root: __dirname });
-//   res.sendFile(path.join(__dirname, "views", "index.html"));
-// });
-
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html"); //302 by default
-});
-
-// Route handlers - middleware
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello World!");
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
   }
-);
-
-// chaining route handlers
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res) => {
-  console.log("three");
-  res.send("Finished!");
-};
-
-app.get("/chain(.html)?", [one, two, three]);
-
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
